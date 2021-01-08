ec826f25c87bf398cd24b08b50a42df4cc3c9d04
Ryunosuke Funasako <e205732@ie.u-ryukyu.ac.jp>
2021-01-08 12:09:48 +0900

    メソッド追加と動作確認

--- app/src/main/java/jp/ac/uryukyu/ie/e205732/HitAndBlow.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205732/HitAndBlow.java
@@ -1,5 +1,8 @@
 package jp.ac.uryukyu.ie.e205732;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class HitAndBlow {
     //ゲーム説明をする出力するメソッド
     public static void direction(){
@@ -8,5 +11,24 @@ public class HitAndBlow {
         + "入力した数字が同じ桁の数字であった場合 eat、\n"
         + "入力した数字が違う桁の数字であった場合 bite とカウントします。\n"
         + "全ての桁が一致すればゲーム終了です。");
+    }
+
+    //答えの配列を用意するメソッド
+    public static void answer(){
+        //答えとなる4つの空の配列answerを用意
+        int[] answer = new int[4];
+        //1~9までの数字を格納する別の配列listを用意
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i <= 9; i++){
+            list.add(i);
+        }
+        //list配列の数字をシャッフル
+        Collections.shuffle(list);
+        //list配列から4つ取り出しanswer配列に入れる
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = list.get(i);
+            //動作確認のため出力させる
+            System.out.println(answer[i]);
+        }
     }
 }

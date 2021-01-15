0e1513e3d01a98de74f9623c1300b375db4d7fc2
Ryunosuke Funasako <e205732@ie.u-ryukyu.ac.jp>
2021-01-15 23:31:07 +0900

    JavaDoc形式でのドキュメントに修正

--- app/src/main/java/jp/ac/uryukyu/ie/e205732/HitAndBlow.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205732/HitAndBlow.java
@@ -6,12 +6,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * HitAndBlowを実装する
+ * 説明文を出力する、答えを用意する、入力を読み込む、判定するメソッドを用意した
+ */
 public class HitAndBlow {
-    //クラス変数の用意
+    /**
+     * クラス変数の用意
+     */
     static int eat = 0;
     static int bite = 0;
     static int turn = 1;
-    //ゲーム説明を出力するメソッド
+
+    /**
+     * ゲーム説明を出力するメソッド
+     */
     public void direction(){
         System.out.println("\n"
         + "---------------Hit&Blow---------------\n"
@@ -21,56 +30,57 @@ public class HitAndBlow {
         + "・ 全ての桁が一致すればゲーム終了です。");
     }
 
-    //答えの配列を用意するメソッド
+    /**
+     * 答えの配列を用意するメソッド
+     * 答えとなる4つの空の配列answerを用意
+     * 0~9が入った配列listを用意
+     * list配列をシャッフルし、0~3番目を配列answerに加える
+     * @return 答えの配列answer
+     */
     public int[] answer(){
-        //答えとなる4つの空の配列answerを用意
         int[] answer = new int[4];
-        //1~9までの数字を格納する別の配列listを用意
         ArrayList<Integer> list = new ArrayList<Integer>();
         for(int i = 0; i <= 9; i++){
             list.add(i);
         }
-        //list配列の数字をシャッフル
         Collections.shuffle(list);
-        //list配列から4つ取り出しanswer配列に入れる
         for(int i = 0; i < answer.length; i++){
             answer[i] = list.get(i);
         }
         return answer;
     }
 
-    //Playerが入力した数字を読み込むメソッド
+    /**
+     * Playerが入力した数字を読み込むメソッド
+     * 入力する4つの空の配列inputを用意
+     * コンソール入力を可能にする
+     * ターン数を表示させる
+     * 入力されたものを数値に変換
+     * 各種例外処理
+     * @return 入力の配列input
+     */
     public int[] input(){
-        //入力する4つの空の配列inputを用意
         int[] input = new int[4];
-        //コンソール入力を可能にする
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
-        //ターン数を表示
         System.out.println("\n" + "***" + turn + "ターン目***");
         for(int j = 0; j < input.length; j++){
             int a = 0;
             System.out.print((j + 1) + "つ目の数字:");
             try{
-                //入力されたものを数値に変換
                 input[j] = Integer.parseInt(br.readLine());
                 a = input[j];
-                //数字以外の文字の入力をされた場合
             }catch(NumberFormatException e){
                 System.out.println("数字を入力してください。");
                 j--;
-                //形式が間違っていた場合
             }catch(IOException e){
                 System.out.println("もう一度入力してください。");
                 j--;
-                //例外があってもなくても必ず実行する処理
             }finally{
-                //入力された数字が範囲外だった場合
                 if(a >= 10){
                     System.out.println("0~9の数字を入力してください。");
                     j--;
                 }
-                //入力された数字が重複していた場合
                 for(int k = j - 1; k >= 0; k--){
                     if(input[j] == input[k]){
                         System.out.println("重複しないように入力してください。");
@@ -82,20 +92,23 @@ public class HitAndBlow {
         return input;
     }
     
-    //判定するメソッド
+    /**
+     * 判定するメソッド
+     * eat,biteの条件
+     * eat,biteの数を表示させる
+     * @param answer 答えの配列answer
+     * @param input 入力の配列input
+     */
     public void judge(int[] answer, int[] input) {
         for(int i = 0; i < answer.length; i++) {
             for(int j = 0; j < input.length; j++) {
-                //eatの条件
                 if(i == j && answer[i] == input[j]) {
                     eat++;
-                    //biteの条件
                 }else if(answer[i] == input[j]){
                     bite++;
                 }
             }
         }
-        //eat,biteのカウンター表示
         System.out.println("\n"
         + "===result===\n" 
         + eat + "eat" + bite + "bite");
--- app/src/main/java/jp/ac/uryukyu/ie/e205732/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205732/Main.java
@@ -1,30 +1,29 @@
 package jp.ac.uryukyu.ie.e205732;
 
+/**
+ * HitAndBlowを動かすためのMainクラス
+ */
 public class Main{
+    /**
+     * HitAndBlowクラスの各種メソッドを呼び出す
+     * ゲーム終了判定等のループ処理
+     * @param args
+     */
     public static void main(String[] args){
-        //HitAndblowインスタンス生成
         HitAndBlow Hit = new HitAndBlow();
-        //説明文出力
         Hit.direction();
-        //judgeメソッドの引数として使えるようにanswerメソッドを呼び出す
         int[] answer;
         answer = Hit.answer();
 
         while(true){
-            //judgeメソッドの引数として使えるようにinputメソッドを呼び出す
             int[] input;
             input = Hit.input();
-            //judgeメソッドを呼び出し、eat数を把握できるようにする
             Hit.judge(answer, input);
-            //ループ処理1回ごとにターンのカウンターを1増やす
             HitAndBlow.turn++;
-            //1ターンごとに判定結果をリセット
             if(HitAndBlow.eat < 4){
                 HitAndBlow.eat = 0;
                 HitAndBlow.bite = 0;
-            }
-            //ゲーム終了判定
-            else if(HitAndBlow.eat == 4){
+            }else if(HitAndBlow.eat == 4){
                 System.out.println("\n"
                 + "~~~congratulations~~~\n"
                 + "4eat達成！ゲーム終了です。\n");
